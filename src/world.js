8321f59e697375dea56d226efa19d56b2cd6fac6
Zekye <Zekye@live.fi>
2017-05-08 21:06:31 +0300

    some model updates

--- src/world.js
+++ src/world.js
@@ -18,11 +18,13 @@ function initWorld(scene) {
 
     player = new Player(scene, loader, Vec3(0, 0, 0), 0.1, 0.1);
 
-    var tree_mat = createColorMaterial(scene, "col1", new BABYLON.Color3(0.4, 0.3, 0.1))
+    // var tree_mat = createColorMaterial(scene, "col1", new BABYLON.Color3(0.4, 0.3, 0.1))
 
     // var dead_tree = loadMesh(loader, "Dead Tree", "DeadTree1.obj", Vec3(0, 0, 0), Vec3(1.0, 1.0, 1.0), tree_mat);
 
-    var tree = loadMesh(loader, "Tree", "Tree1.obj", Vec3(0, 0, 0), Vec3(1.0, 1.0, 1.0), tree_mat);
+    // var tree = loadMesh(loader, "Tree", "lowpoyltree.obj", Vec3(0, 0, 0), Vec3(1.0, 1.0, 1.0), tree_mat);
+
+    var tree = new Tree(scene, loader, Vec3(0, 0, 0), Vec3(1, 1, 1), "lowpoyltree.obj", "TreeBase.jpg");
 
     loader.load();
 
@@ -31,7 +33,7 @@ function initWorld(scene) {
 function updateWorld(scene) {
     player.movement();
     ground.update(function (mesh) {
-        player.setY(player.getDistance(ground.maximum.y, mesh) + player.maximum.y / 2);
+        if (player.loaded) player.setY(player.getDistance(ground.maximum.y, mesh) + player.maximum.y / 2);
     });
 }
 
@@ -46,5 +48,17 @@ class Ground {
         if (!this.loaded) return;
         task(this.mesh);
     }
+}
 
+class Tree {
+    constructor(scene, loader, pos, size, obj, texture) {
+        this.material = createTextureMaterial(scene, "Tree_Texture", texture, Vec2(1.0, 1.0));
+        this.model = loadMesh(loader, "Tree", obj, pos, size, this.material);
+        this.model.then((e) => (this.mesh = e, this.maximum = e.getBoundingInfo().maximum, this.loaded = true));
+    }
+
+    update(task) {
+        if (!this.loaded) return;
+        task(this.mesh);
+    }
 }
